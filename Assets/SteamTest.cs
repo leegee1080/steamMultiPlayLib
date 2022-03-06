ec11318ebda429f7fe7dcc653ec5b83fed7dde8b
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-03-06 09:09:27 -0500

    init

---
+++ Assets/SteamTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!SteamManager.Initialized) {return;}
+        string name = SteamFriends.GetPersonaName();
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
